7fc7d54d09150da9bb1a9a3d78a30cf15cfd5d85
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-09-28 09:08:44 +0300

    add final variable

--- src/main/java/br/com/leonardoz/features/synchronizers/UsingBarriers.java
+++ src/main/java/br/com/leonardoz/features/synchronizers/UsingBarriers.java
@@ -8,39 +8,61 @@ import java.util.concurrent.Executors;
 /**
  * Barriers are used for blocking a group of threads until they come together at
  * a single point in order to proceed. Basically, convergence of threads.
- * 
+ * <p>
  * Accepts a runnable in it's constructor to be called when the threads reach the
  * barrier, but before its unblocked
- * 
+ * <p>
  * Most common implementation is cyclic barrier.
- * 
  */
 public class UsingBarriers {
 
-	public static void main(String[] args) {
+    private static final int parties = 10;
 
-		Runnable barrierAction = () -> System.out.println("Well done, guys!");
+    public static void main(String[] args) {
 
-		var executor = Executors.newCachedThreadPool();
-		var barrier = new CyclicBarrier(8, barrierAction);
+        Runnable afterPerformBarrier = () -> System.out.println("Well done");
 
-		Runnable task = () -> {
-			try {
-				// simulating a task that can take at most 1sec to run
-				System.out.println("Doing task for " + Thread.currentThread().getName());
-				Thread.sleep(new Random().nextInt(10) * 100);
-				System.out.println("Done for " + Thread.currentThread().getName());
-				barrier.await();
-			} catch (InterruptedException | BrokenBarrierException e) {
-				e.printStackTrace();
-			}
-		};
+        var executor = Executors.newCachedThreadPool();
+        var barrier = new CyclicBarrier(parties, afterPerformBarrier);
+        Runnable task = () -> {
 
-		for (int i = 0; i < 10; i++) {
-			executor.execute(task);
-		}
-		executor.shutdown();
+            try {
+                System.out.println(Thread.currentThread().getName() + "started!");
+                Thread.sleep(new Random(1000).nextInt(1222) * 10);
+                System.out.println(Thread.currentThread().getName() + "ended!");
+                barrier.await();
+            } catch (Exception e) {
+                System.err.println("Exception!");
+            }
+        };
 
-	}
+        for (int i = 0; i < parties; i++) {
+            executor.execute(task);
+        }
+    }
+
+    private static void doAction() {
+        Runnable barrierAction = () -> System.out.println("Well done, guys!");
+
+        var executor = Executors.newCachedThreadPool();
+        var barrier = new CyclicBarrier(5, barrierAction);
+
+        Runnable task = () -> {
+            try {
+                // simulating a task that can take at most 1sec to run
+                System.out.println("Doing task for " + Thread.currentThread().getName());
+                Thread.sleep(new Random().nextInt(10) * 100);
+                System.out.println("Done for " + Thread.currentThread().getName());
+                barrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        };
+
+        for (int i = 0; i < parties; i++) {
+            executor.execute(task);
+        }
+        executor.shutdown();
+    }
 
 }
